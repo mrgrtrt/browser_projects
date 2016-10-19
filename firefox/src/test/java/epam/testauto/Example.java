94ba0600a90f2c45d6c0463210e164d25468077b
Маргарита Иванова < >
2016-10-19 15:13:45 +0300

    added pageobject

--- firefox/src/test/java/epam/testauto/Example.java
+++ firefox/src/test/java/epam/testauto/Example.java
@@ -4,15 +4,16 @@ package epam.testauto;
  * Created by Rita on 09.10.2016.
  */
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriverException;
+import epam.controls.Checkbox;
+import epam.controls.Login;
+import epam.controls.RadioButton;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
 public class Example {
-    private FirefoxDriver firefox;
+    private WebDriver firefox;
 
     @BeforeSuite
     public void setup() {
@@ -28,27 +29,71 @@ public class Example {
     }
 
     @Test(priority = 1, dataProviderClass = Data.class, dataProvider = "login")
-    public void authorization(String sUsername, String sPassword) {
-        firefox.findElement(By.cssSelector(".profile-photo")).click();
+    public void authorization(boolean bool, String sUsername, String sPassword) {
+        /*firefox.findElement(By.cssSelector(".profile-photo")).click();
         firefox.findElement(By.id("Login")).sendKeys(sUsername);
-        firefox.findElement(By.id("Password")).sendKeys(sPassword + Keys.ENTER);
-        
+        firefox.findElement(By.id("Password")).sendKeys(sPassword);
+        firefox.findElement(By.cssSelector("button.btn-login")).click();
+
         try {
-            Assert.assertTrue(firefox.findElement(By.cssSelector(".logout")).isEnabled());
             firefox.navigate().to("/tests/page1.htm");
             Assert.assertEquals(firefox.getTitle(), "Contact Form");
-            System.out.println("Succesfully submitted");
+            if (bool == "Contact Form".equals(firefox.getTitle())) {
+                System.out.println("Succesfully submitted");
+            }
         }
         catch (AssertionError e) {
             System.out.println("Login failed, invalid username or password");
-        }
-		
-        /*Assert.assertTrue(firefox.findElement(By.id("Name")).isDisplayed());
-        Assert.assertTrue(firefox.findElement(By.id("LastName")).isDisplayed());
-        Assert.assertTrue(firefox.findElement(By.id("Description")).isDisplayed());*/
+        }*/
+
+        Login login = new Login(firefox, ".profile-photo");
+        login.clickToStart();
+        login.typeData(sUsername, sPassword);
+        login.submit();
+        login.loginCheck(bool);
     }
 
     @Test(priority = 2)
+    public void workingWPageObject() throws InterruptedException {
+        firefox.navigate().to("/tests/page8.htm");
+        Assert.assertEquals(firefox.getTitle(), "Different Element");
+
+        Checkbox water = new Checkbox(firefox, "Water");
+        Checkbox earth = new Checkbox(firefox, "Earth");
+        Checkbox wind = new Checkbox(firefox, "Wind");
+        Checkbox fire = new Checkbox(firefox, "Fire");
+        water.check();
+        earth.check();
+        wind.check();
+        fire.check();
+        Assert.assertTrue(water.isChecked());
+        Assert.assertTrue(earth.isChecked());
+        Assert.assertTrue(wind.isChecked());
+        Assert.assertTrue(fire.isChecked());
+        water.uncheck();
+        earth.uncheck();
+        wind.uncheck();
+        fire.uncheck();
+        Assert.assertFalse(water.isChecked());
+        Assert.assertFalse(earth.isChecked());
+        Assert.assertFalse(wind.isChecked());
+        Assert.assertFalse(fire.isChecked());
+
+        RadioButton gold = new RadioButton(firefox, "Gold");
+        RadioButton silver = new RadioButton(firefox, "Silver");
+        RadioButton bronze = new RadioButton(firefox, "Bronze");
+        RadioButton selen = new RadioButton(firefox, "Selen");
+        gold.check();
+        Assert.assertTrue(gold.isChecked());
+        silver.check();
+        Assert.assertTrue(silver.isChecked());
+        bronze.check();
+        Assert.assertTrue(bronze.isChecked());
+        selen.check();
+        Assert.assertTrue(selen.isChecked());
+    }
+
+    @Test(priority = 3)
     //generate js alert and handle it
     public void handleAlert() throws WebDriverException{
         JavascriptExecutor js = (JavascriptExecutor) firefox;
